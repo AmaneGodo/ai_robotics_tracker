69ad3cb55d72061195de13a7116904797fef8e35
Amane Godo <amanegodo@gmail.com>
2026-02-03 16:21:18 -0500

    decreased the k value for controller input
    decreased the k value from -0.1 to -0.05 to study closed loop behavior:
    
    Predicted that a smaller gain would weaken feedback, slowing velocity reversal and increasing overshoot.
    Observed that the system responded more gently, took longer to converge, and overshot further due to delayed momentum correction.

--- src/robotics/Project3_cpp_estimation_control/src/Controller.cpp
+++ src/robotics/Project3_cpp_estimation_control/src/Controller.cpp
@@ -5,7 +5,7 @@ Controller::Controller() {}
 
 double Controller::update(const State& estimated_state) {
     // Dummy control: push toward zero
-    double control = -0.1 * estimated_state.position;
+    double control = -0.05 * estimated_state.position;
 
     std::cout << "[Controller] u=" << control << "\n";
     return control;
